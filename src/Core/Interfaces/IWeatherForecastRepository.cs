0471dcf66c639b2ef41b7d3201c96d05534c4861
grantcolley <grantcolley@yahoo.com>
2022-05-08 07:54:07 +0100

    Delete solution files

--- src/Core/Interfaces/IWeatherForecastRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using Core.Models;
-
-namespace Core.Interfaces
-{
-    public interface IWeatherForecastRepository
-    {
-        IEnumerable<WeatherForecast> GetWeatherForecasts();
-    }
-}
--- src/Core/Interfaces/IWeatherForecastService.cs
+++
@@ -1,9 +0,0 @@
-﻿using Core.Models;
-
-namespace Core.Interfaces
-{
-    public interface IWeatherForecastService
-    {
-        Task<IEnumerable<WeatherForecast>> GetWeatherForecasts();
-    }
-}
