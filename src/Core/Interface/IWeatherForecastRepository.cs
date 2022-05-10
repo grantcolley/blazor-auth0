b4200aef93cbe271ae754d32888782e4d9e5d95e
grantcolley <grantcolley@yahoo.com>
2022-05-10 20:38:20 +0100

    Add Core.csproj

---
+++ src/Core/Interface/IWeatherForecastRepository.cs
@@ -0,0 +1,10 @@
+﻿using Core.Model;
+using System.Collections.Generic;
+
+namespace Core.Interface
+{
+    public interface IWeatherForecastRepository
+    {
+        IEnumerable<WeatherForecast> GetWeatherForecasts();
+    }
+}
---
+++ src/Core/Interface/IWeatherForecastService.cs
@@ -0,0 +1,11 @@
+﻿using Core.Model;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Core.Interface
+{
+    public interface IWeatherForecastService
+    {
+        Task<IEnumerable<WeatherForecast>> GetWeatherForecasts();
+    }
+}
