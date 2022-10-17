acd040c57b41a610f00b15c85d3bf69d8ab375f1
grantcolley <grantcolley@yahoo.com>
2022-10-17 13:10:57 +0100

    Update Program.cs

--- src/BlazorServerApp/Program.cs
+++ src/BlazorServerApp/Program.cs
@@ -15,7 +15,7 @@ JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
 builder.Services
     .AddAuth0WebAppAuthentication(Auth0Constants.AuthenticationScheme, options =>
     {
-        options.Domain = builder.Configuration["Auth0:Domain"];
+        options.Domain = builder.Configuration["Auth0:Authority"];
         options.ClientId = builder.Configuration["Auth0:ClientId"];
         options.ClientSecret = builder.Configuration["Auth0:ClientSecret"];
         options.ResponseType = "code";
