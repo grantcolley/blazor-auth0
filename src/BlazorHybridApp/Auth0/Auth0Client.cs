c2e97de5bcf92011fb0ac1f246e7d53667a940ca
grantcolley <grantcolley@yahoo.com>
2022-10-18 20:22:31 +0100

    Add BlazorHybridApp to solution

---
+++ src/BlazorHybridApp/Auth0/Auth0Client.cs
@@ -0,0 +1,73 @@
+﻿using IdentityModel.Client;
+using IdentityModel.OidcClient;
+using IdentityModel.OidcClient.Browser;
+using System.Collections.Generic;
+using System;
+using System.Threading.Tasks;
+using Core.Model;
+
+namespace BlazorHybridApp.Auth0
+{
+    public class Auth0Client
+    {
+        private readonly OidcClient oidcClient;
+        private readonly TokenProvider tokenProvider;
+
+        public Auth0Client(Auth0ClientOptions options, TokenProvider tokenProvider)
+        {
+            oidcClient = new OidcClient(new OidcClientOptions
+            {
+                Authority = $"https://{options.Domain}",
+                ClientId = options.ClientId,
+                Scope = options.Scope,
+                RedirectUri = options.RedirectUri,
+                Browser = options.Browser
+            });
+
+            this.tokenProvider = tokenProvider;
+        }
+
+        public IdentityModel.OidcClient.Browser.IBrowser Browser
+        {
+            get
+            {
+                return oidcClient.Options.Browser;
+            }
+            set
+            {
+                oidcClient.Options.Browser = value;
+            }
+        }
+
+        public async Task<LoginResult> LoginAsync()
+        {
+            var loginResult = await oidcClient.LoginAsync();
+            tokenProvider.RefreshToken = loginResult.RefreshToken;
+            tokenProvider.AccessToken = loginResult.AccessToken;
+            tokenProvider.IdToken = loginResult.IdentityToken;
+            return loginResult;
+        }
+
+        public async Task<BrowserResult> LogoutAsync()
+        {
+            var logoutParameters = new Dictionary<string, string>
+            {
+                {"client_id", oidcClient.Options.ClientId },
+                {"returnTo", oidcClient.Options.RedirectUri }
+            };
+
+            var logoutRequest = new LogoutRequest();
+            var endSessionUrl = new RequestUrl($"{oidcClient.Options.Authority}/v2/logout")
+              .Create(new Parameters(logoutParameters));
+            var browserOptions = new BrowserOptions(endSessionUrl, oidcClient.Options.RedirectUri)
+            {
+                Timeout = TimeSpan.FromSeconds(logoutRequest.BrowserTimeout),
+                DisplayMode = logoutRequest.BrowserDisplayMode
+            };
+
+            var browserResult = await oidcClient.Options.Browser.InvokeAsync(browserOptions);
+
+            return browserResult;
+        }
+    }
+}
---
+++ src/BlazorHybridApp/MauiProgram.cs
@@ -0,0 +1,62 @@
+﻿using BlazorHybridApp.Auth0;
+using Core.Interface;
+using Core.Model;
+using Microsoft.AspNetCore.Components.WebView.Maui;
+using Services;
+
+namespace BlazorHybridApp
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                });
+
+            builder.Services.AddMauiBlazorWebView();
+
+            #if DEBUG
+		    builder.Services.AddBlazorWebViewDeveloperTools();
+            #endif
+
+            builder.Services.AddSingleton<TokenProvider>();
+
+            builder.Services.AddSingleton(sp =>
+            {
+                var tokenProvider = sp.GetRequiredService<TokenProvider>();
+                return new Auth0Client(new Auth0ClientOptions
+                {
+                    Domain = "<YOUR_AUTH0_DOMAIN>",
+                    ClientId = "<YOUR_CLIENT_ID>",
+                    Scope = "openid profile",
+
+                    // https://github.com/dotnet/maui/issues/8382
+                    // RedirectUri = "http://localhost/callback"
+
+                    RedirectUri = "myapp://callback"
+
+                }, tokenProvider);
+            });
+
+            builder.Services.AddHttpClient("webapi", client =>
+            {
+                client.BaseAddress = new Uri("https://localhost:7225");
+            });
+
+            builder.Services.AddTransient<IWeatherForecastService, WeatherForecastService>(sp =>
+            {
+                var tokenProvider = sp.GetRequiredService<TokenProvider>();
+                var httpClientFactory = sp.GetRequiredService<IHttpClientFactory>();
+                var httpClient = httpClientFactory.CreateClient("webapi");
+                return new WeatherForecastService(httpClient, tokenProvider);
+            });
+
+            return builder.Build();
+        }
+    }
+}
